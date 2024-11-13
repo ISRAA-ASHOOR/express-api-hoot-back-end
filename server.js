49ad60f5b25138c049ecbd9a4dba8d6d62263dcf
israa <esraashoor0112@gmail.com>
2024-11-13 18:06:12 +0300

    Completed Back End

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const testJWTRouter = require('./controllers/test-jwt');
 const usersRouter = require('./controllers/users');
 const profilesRouter = require('./controllers/profiles');
 const cors = require('cors');
+const hootsRouter = require('./controllers/hoots.js');
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -24,6 +25,7 @@ app.use(morgan('dev'));
 app.use('/test-jwt', testJWTRouter);
 app.use('/users', usersRouter);
 app.use('/profiles', profilesRouter);
+app.use('/hoots', hootsRouter);
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
